61c0cc1c824f52b08793730c1f4e7c47e4b7ca80
Artenes Nogueira <artenesama@gmail.com>
2019-10-03 19:38:36 -0400

    Validates data on form submit
    By using Yup is possible to write some validation logic to apply in the form.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('name is required'),
+  email: Yup.string()
+    .email('insert valid e-mail')
+    .required('e-mail is required'),
+  password: Yup.string()
+    .min(6, 'at least 6 characters long')
+    .required('password is required'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -12,7 +23,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="Your e-mail" />
         <Input
