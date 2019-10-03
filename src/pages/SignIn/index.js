dd41e4105469dfec78317152674ac2b86141c733
Artenes Nogueira <artenesama@gmail.com>
2019-10-03 19:25:44 -0400

    Uses unform library to build html forms
    Just some useful tool made by people at rocketseat.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
         <button type="submit">Access</button>
         <Link to="/register">Create a free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Full name" />
-        <input type="email" placeholder="Your e-mail" />
-        <input type="password" placeholder="Your secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Your secret password"
+        />
         <button type="submit">Create account</button>
         <Link to="/">I have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
