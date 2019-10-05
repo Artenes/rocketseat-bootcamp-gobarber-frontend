2260e9ce7a7fe7e1b251d95067cc1075151318d0
Artenes Nogueira <artenesama@gmail.com>
2019-10-05 09:05:52 -0400

    Shows toast on login failure

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -17,7 +18,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('User is not provider');
+      toast.error('User is not provider');
       return;
     }
 
@@ -25,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Login failure, check your data again');
     yield put(signFailure());
   }
 }
