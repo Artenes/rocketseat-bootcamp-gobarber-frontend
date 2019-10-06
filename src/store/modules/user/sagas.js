6c077eaea9b5153d5e2bce6d5ee33ae203cf4743
Artenes Nogueira <artenesama@gmail.com>
2019-10-06 10:36:33 -0400

    Updates user profile on backend

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,26 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, put, call } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import { updateProfileFailure, updateProfileSuccess } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload;
+
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile updated');
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error('Error while updating profile. Check your data again.');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
