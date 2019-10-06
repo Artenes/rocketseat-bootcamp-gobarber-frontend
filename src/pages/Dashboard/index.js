912e5a7bbf6bb8f32a64f0ccdc01f94a81d187a9
Artenes Nogueira <artenesama@gmail.com>
2019-10-06 12:17:13 -0400

    Creates layout for appointments in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>May, 31</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Reachel Miller</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Open</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Hamona Jennifer</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Monique Siqueira</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
